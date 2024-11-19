231cdbcbdd54c5aa696e0ad38b15f1551d1d335c
Jakub Chojdak <jchojdak@gmail.com>
2024-11-19 21:47:16 +0100

    Added integration tests for deleteUserById endpoint in UserController

--- src/test/java/com/forumengine/user/UserIntegrationTest.java
+++ src/test/java/com/forumengine/user/UserIntegrationTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.LocalDateTime;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,6 +32,8 @@ public class UserIntegrationTest extends IntegrationTestConfig {
     private static final boolean USER_ACTIVE = true;
     private static final boolean USER_BLOCKED = false;
     private static final Long INVALID_USER_ID = 404L;
+
+    private static final String ROLE_ADMIN = "ADMIN";
 
     private User testUser;
 
@@ -91,4 +95,33 @@ public class UserIntegrationTest extends IntegrationTestConfig {
         result.andExpect(status().is(404));
     }
 
+    @Test
+    @Transactional
+    @WithMockUser(roles = ROLE_ADMIN)
+    void shouldReturn200_whenDeleteUserById() throws Exception {
+        // given
+        Long userId = testUser.getId();
+
+        // when
+        ResultActions result = mockMvc.perform(delete(ENDPOINT.formatted(userId))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().is(200));
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(roles = ROLE_ADMIN)
+    void shouldReturn404_whenDeleteUserByIdWithInvalidData() throws Exception {
+        // given
+        Long userId = INVALID_USER_ID;
+
+        // when
+        ResultActions result = mockMvc.perform(delete(ENDPOINT.formatted(userId))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().is(404));
+    }
 }
