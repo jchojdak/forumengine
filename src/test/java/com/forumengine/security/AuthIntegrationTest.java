5cfbc1dc2b15b8090ee1e7f6aedc644106bfe039
Jakub Chojdak <jchojdak@gmail.com>
2024-11-21 19:41:47 +0100

    Added integration tests for change password endpoint in AuthController

--- src/test/java/com/forumengine/security/AuthIntegrationTest.java
+++ src/test/java/com/forumengine/security/AuthIntegrationTest.java
@@ -2,15 +2,24 @@ package com.forumengine.security;
 
 import com.forumengine.IntegrationTestConfig;
 import com.forumengine.TestUtils;
+import com.forumengine.security.dto.ChangePasswordRequest;
 import com.forumengine.security.dto.LoginUserDTO;
 import com.forumengine.security.dto.RegisterUserDTO;
+import com.forumengine.user.User;
+import com.forumengine.user.UserRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -19,6 +28,7 @@ public class AuthIntegrationTest extends IntegrationTestConfig {
 
     private static final String ENDPOINT_REGISTER = "/auth/register";
     private static final String ENDPOINT_LOGIN = "/auth/login";
+    private static final String ENDPOINT_CHANGE_PASSWORD = "/auth/password";
 
     private static final String ROLE_USER = "ROLE_USER";
 
@@ -38,6 +48,12 @@ public class AuthIntegrationTest extends IntegrationTestConfig {
     private static final String FOURTH_USER_PASSWORD = "password";
     private static final String FOURTH_USER_EMAIL = "adam.smith@gmail.com";
 
+    private static final String FIFTH_USER_USERNAME = "testUser123";
+    private static final String FIFTH_USER_OLD_PASSWORD = "oldPassword";
+    private static final String FIFTH_USER_INVALID_OLD_PASSWORD = "OooldddPassw0rd123";
+    private static final String FIFTH_USER_NEW_PASSWORD = "newPassword";
+    private static final String FIFTH_USER_EMAIL = "test.user@gmail.com";
+
     private static final String DUPLICATE_USER_USERNAME = "ArthurMurphy456";
     private static final String DUPLICATE_USER_EMAIL = "felix.smith@gmail.com";
 
@@ -47,6 +63,12 @@ public class AuthIntegrationTest extends IntegrationTestConfig {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
     @Test
     void shouldReturn200AndToken_whenUserLogsInWithValidData() throws Exception {
@@ -158,6 +180,58 @@ public class AuthIntegrationTest extends IntegrationTestConfig {
         // then
         result.andExpect(status().is(409))
                 .andExpect(jsonPath("$.message").value(ALREADY_EXIST_MESSAGE.formatted(FOURTH_USER_USERNAME)));
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(username = FIFTH_USER_USERNAME)
+    void shouldReturn200_whenPasswordIsChanged() throws Exception {
+        // given
+        String encodedPassword = encoder.encode(FIFTH_USER_OLD_PASSWORD);
+
+        ChangePasswordRequest request = new ChangePasswordRequest();
+        request.setOldPassword(FIFTH_USER_OLD_PASSWORD);
+        request.setNewPassword(FIFTH_USER_NEW_PASSWORD);
+
+        User user = new User();
+        user.setUsername(FIFTH_USER_USERNAME);
+        user.setEmail(FIFTH_USER_EMAIL);
+        user.setPassword(encodedPassword);
+        userRepository.save(user);
+
+        // when
+        ResultActions result = mockMvc.perform(patch(ENDPOINT_CHANGE_PASSWORD)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtils.asJsonString(request)));
+
+        // then
+        result.andExpect(status().is(200));
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(username = FIFTH_USER_USERNAME)
+    void shouldReturn400_whenPasswordIsNotChangedWithInvalidOldPassword() throws Exception {
+        // given
+        String encodedPassword = encoder.encode(FIFTH_USER_OLD_PASSWORD);
+
+        ChangePasswordRequest request = new ChangePasswordRequest();
+        request.setOldPassword(FIFTH_USER_INVALID_OLD_PASSWORD);
+        request.setNewPassword(FIFTH_USER_NEW_PASSWORD);
+
+        User user = new User();
+        user.setUsername(FIFTH_USER_USERNAME);
+        user.setEmail(FIFTH_USER_EMAIL);
+        user.setPassword(encodedPassword);
+        userRepository.save(user);
+
+        // when
+        ResultActions result = mockMvc.perform(patch(ENDPOINT_CHANGE_PASSWORD)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtils.asJsonString(request)));
+
+        // then
+        result.andExpect(status().is(400));
     }
 
 }
