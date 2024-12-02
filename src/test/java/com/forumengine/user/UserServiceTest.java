ca78bd99a7b31065fd1b82509c1773f05648c418
Jakub Chojdak <jchojdak@gmail.com>
2024-12-02 22:32:31 +0100

    UserService: UpdateUserByName method: added unit tests

--- src/test/java/com/forumengine/user/UserServiceTest.java
+++ src/test/java/com/forumengine/user/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.forumengine.user;
 
 import com.forumengine.exception.EntityNotFoundException;
+import com.forumengine.user.dto.UpdateUserRequest;
 import com.forumengine.user.dto.UserDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,10 +23,15 @@ public class UserServiceTest {
     private static final Long USER_ID = 1L;
     private static final Long INVALID_USER_ID = 404L;
     private static final String USER_USERNAME = "johnDoe123";
+    private static final String INVALID_USERNAME = "inve21iei21iscni23i2iend";
     private static final String USER_FIRSTNAME = "John";
+    private static final String NEW_FIRSTNAME = "Johny";
     private static final String USER_LASTNAME = "Doe";
+    private static final String NEW_LASTNAME = "Smith";
     private static final String USER_EMAIL = "john.doe@gmail.com";
+    private static final String NEW_EMAIL = "johny.smith@gmail.com";
     private static final String USER_MOBILE = "123456789";
+    private static final String NEW_MOBILE = "+48123321123";
     private static final String USER_PASSWORD = "$2a$10$JRF3ZFONOZhEZbC62I66eOtte8r1d4PRf0SZVcrIqkiB2rNLRn/fK";
     private static final boolean USER_ACTIVE = true;
     private static final boolean USER_BLOCKED = false;
@@ -87,7 +93,7 @@ public class UserServiceTest {
     }
 
     @Test
-    void testGetUserById_throwEntityNotException_whenUserNotFound() {
+    void testGetUserById_throwsEntityNotException_whenUserNotFound() {
         // given
         when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
 
@@ -118,7 +124,7 @@ public class UserServiceTest {
     }
 
     @Test
-    void testDeleteUserById_throwEntityNotException_whenUserNotFound() {
+    void testDeleteUserById_throwsEntityNotException_whenUserNotFound() {
         // given
         when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
 
@@ -132,5 +138,60 @@ public class UserServiceTest {
         assertEquals(NOT_FOUND_MESSAGE.formatted(INVALID_USER_ID), result.getMessage());
 
         verify(userRepository).findById(anyLong());
+    }
+
+    @Test
+    void testUpdateUserByName() {
+        // given
+        UpdateUserRequest request = new UpdateUserRequest();
+        request.setFirstName(NEW_FIRSTNAME);
+        request.setLastName(NEW_LASTNAME);
+        request.setMobile(NEW_MOBILE);
+        request.setEmail(NEW_EMAIL);
+
+        User updatedUser = testUser;
+        updatedUser.setFirstName(NEW_FIRSTNAME);
+        updatedUser.setLastName(NEW_LASTNAME);
+        updatedUser.setMobile(NEW_MOBILE);
+        updatedUser.setEmail(NEW_EMAIL);
+
+        UserDTO updatedUserDTO = testUserDTO;
+        updatedUserDTO.setFirstName(NEW_FIRSTNAME);
+        updatedUserDTO.setLastName(NEW_LASTNAME);
+        updatedUserDTO.setMobile(NEW_MOBILE);
+        updatedUserDTO.setEmail(NEW_EMAIL);
+
+        when(userRepository.findByUsername(USER_USERNAME)).thenReturn(Optional.of(testUser));
+        when(userRepository.save(any(User.class))).thenReturn(updatedUser);
+        when(userMapper.toDTO(any(User.class))).thenReturn(updatedUserDTO);
+
+        // when
+        UserDTO result = userService.updateUserByName(USER_USERNAME, request);
+
+        // then
+        assertNotNull(result);
+        assertEquals(NEW_FIRSTNAME, result.getFirstName());
+        assertEquals(NEW_LASTNAME, result.getLastName());
+        assertEquals(NEW_MOBILE, result.getMobile());
+        assertEquals(NEW_EMAIL, result.getEmail());
+
+        verify(userRepository).findByUsername(USER_USERNAME);
+        verify(userRepository).save(any(User.class));
+        verify(userMapper).toDTO(any(User.class));
+    }
+
+    @Test
+    void testUpdateUserByName_throwsEntityNotFoundException_whenUserIsNotFound() {
+        // given
+        UpdateUserRequest request = new UpdateUserRequest();
+        request.setFirstName(NEW_FIRSTNAME);
+        request.setLastName(NEW_LASTNAME);
+        request.setMobile(NEW_MOBILE);
+        request.setEmail(NEW_EMAIL);
+
+        // when & then
+        assertThrows(EntityNotFoundException.class, () -> {
+            userService.updateUserByName(INVALID_USERNAME, request);
+        });
     }
 }
